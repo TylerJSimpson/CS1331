58de445c8a52a5c38d55fe2b32a6e39d8c6448de
TylerJSimpson <simpsontylerjames@gmail.com>
2025-03-27 10:27:56 -0400

    started module 8 homework

--- 3_exceptions_data_structures_recursion_guis/module_8/homework/Dog.java
+++ 3_exceptions_data_structures_recursion_guis/module_8/homework/Dog.java
@@ -1,3 +1,37 @@
-public class Dog {
+public class Dog extends Pet {
     
+    // Variables
+
+    private double droolRate;
+
+    // Constructors
+
+    public Dog(String name, double health, int painLevel) {
+
+        super(name, health, painLevel);
+        this.droolRate = 5.0;
+    }
+
+    public Dog(String name, double health, int painLevel, double droolRate) {
+        
+        super(name, health, painLevel);
+        // If droolRate is <= 0 set to 0.5
+        if (droolRate <= 0) {
+            this.droolRate = 0.5;
+        } else {
+            this.droolRate = droolRate;
+        }
+    }
+
+    // Getters
+
+    public double getDroolRate() {
+        return droolRate;
+    }
+
+    // Methods
+
+    
+
+
 }
--- 3_exceptions_data_structures_recursion_guis/module_8/homework/Pet.java
+++ 3_exceptions_data_structures_recursion_guis/module_8/homework/Pet.java
@@ -1,3 +1,77 @@
-public class Pet {
-    
+public abstract class Pet {
+
+    // Variables
+
+    private String name;
+    private double health;
+    private int painLevel;
+
+    // Constructors
+
+    public Pet(String name, double health, int painLevel) {
+
+        this.name = name;
+
+        // Ensure health is between 0.0 and 1.0
+        if (health <= 1.0 && health >= 0.0) {
+            this.health = health;
+        } else if (health > 1.0) {
+            this.health = 1.0;
+        } else {
+            this.health = 0.0;
+        }
+
+        // Ensure painLevel is between 1 and 10
+        if (painLevel <= 10 && painLevel >= 1) {
+            this.painLevel = painLevel;
+        } else if (painLevel > 10) {
+            this.painLevel = 10;
+        } else {
+            this.painLevel = 1;
+        }
+    }
+
+    // Getters
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public int getPainLevel() {
+        return painLevel;
+    }
+
+    // Methods
+
+    public abstract int treat();
+
+    public void speak() {
+        
+        String baseString = "Hello! My name is ";
+        // Print Pet's name, uppercase if painLevel > 5
+        if (this.painLevel > 5) {
+            System.out.println(baseString.toUpperCase() + this.name.toUpperCase());
+        } else {
+            System.out.println(baseString + this.name);
+        }
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Pet) {
+            Pet pet = (Pet) o;
+            return this.name.equals(pet.name);
+        }
+        return false;
+    }
+
+    protected void heal() {
+        this.health = 1.0;
+        this.painLevel = 1;
+    }
+
+
 }
