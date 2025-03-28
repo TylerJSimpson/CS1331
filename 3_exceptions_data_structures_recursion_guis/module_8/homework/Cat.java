e087186ccd46b39881b54fb3332e3e63696e6e4d
TylerJSimpson <simpsontylerjames@gmail.com>
2025-03-28 08:56:10 -0400

    homework progress finished Dog and Cat classes

--- 3_exceptions_data_structures_recursion_guis/module_8/homework/Cat.java
+++ 3_exceptions_data_structures_recursion_guis/module_8/homework/Cat.java
@@ -1,3 +1,79 @@
-public class Cat {
-    
+public class Cat extends Pet {
+
+    // Variables
+
+    private int miceCaught;
+
+    // Constructors
+
+    public Cat(String name, double health, int painLevel) {
+        super(name, health, painLevel);
+        this.miceCaught = 0;
+
+    }
+
+    public Cat(String name, double health, int painLevel, int miceCaught) {
+        super(name, health, painLevel);
+
+        if (miceCaught < 0) {
+            this.miceCaught = 0;
+        } else {
+            this.miceCaught = miceCaught;
+        }
+
+    }
+
+    // Getters
+    public int getMiceCaught() {
+        return miceCaught;
+    }
+
+    // Methods
+
+    @Override
+    public int treat() {
+
+        double calculatedMinutes; 
+
+        if (miceCaught < 4) {
+            calculatedMinutes = ((getPainLevel() * 2.0 ) / getHealth());
+        } else if (miceCaught <= 7) {
+            calculatedMinutes = (getPainLevel() / getHealth());
+        } else {
+            calculatedMinutes = (getPainLevel() / (getHealth() * 2.0));
+        }
+
+        int treatmentMinutes = (int) Math.ceil(calculatedMinutes);
+
+        heal();
+
+        return treatmentMinutes;
+
+    }
+
+    @Override
+    public void speak() {
+        super.speak();
+        
+        StringBuilder meow = new StringBuilder();
+        String meowSound = getPainLevel() > 5 ? "MEOW" : "meow";
+        
+        for (int i = 0; i < miceCaught; i++) {
+            meow.append(meowSound);
+            if (i < miceCaught - 1) {
+                meow.append(" ");
+            }
+        }
+        
+        System.out.println(meow.toString());
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Cat) {
+            Cat cat = (Cat) o;
+            return super.equals(o) && this.miceCaught == cat.miceCaught;
+        }
+        return false;
+    }
+
 }
--- 3_exceptions_data_structures_recursion_guis/module_8/homework/Dog.java
+++ 3_exceptions_data_structures_recursion_guis/module_8/homework/Dog.java
@@ -31,7 +31,53 @@ public class Dog extends Pet {
 
     // Methods
 
-    
+    @Override
+    public int treat() {
+
+        double calculatedMinutes; 
+
+        if (droolRate < 3.5) {
+            calculatedMinutes = ((getPainLevel() * 2.0 ) / getHealth());
+        } else if (droolRate <= 7.5) {
+            calculatedMinutes = (getPainLevel() / getHealth());
+        } else {
+            calculatedMinutes = (getPainLevel() / (getHealth() * 2.0));
+        }
+
+        int treatmentMinutes = (int) Math.ceil(calculatedMinutes);
+
+        heal();
+
+        return treatmentMinutes;
+    }
+
+    @Override
+    public void speak() {
+        super.speak();
+        
+        StringBuilder bark = new StringBuilder();
+        String barkSound = getPainLevel() > 5 ? "BARK" : "bark";
+        
+        for (int i = 0; i < getPainLevel(); i++) {
+            bark.append(barkSound);
+            if (i < getPainLevel() - 1) {
+                bark.append(" ");
+            }
+        }
+        
+        System.out.println(bark.toString());
+    }
 
 
-}
+    public boolean equals(Object o) {
+        if (o instanceof Dog) {
+            Dog dog = (Dog) o;
+            return super.equals(o) && this.droolRate == dog.droolRate;
+        }
+        return false;
+    }
+
+
+
+    }
+
